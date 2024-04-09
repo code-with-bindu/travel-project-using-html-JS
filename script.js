df000fd28b24ea8dcb647b2c04796c49d33937c9
hima bindu <121006526+bindureddy-coder@users.noreply.github.com>
2024-04-09 22:20:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+let searchBtn = document.querySelector('#search-btn');
+let searchBar = document.querySelector('.search-bar-container');
+let formBtn = document.querySelector('#login-btn');
+let loginform = document.querySelector('.login-form-container');
+let formclose = document.querySelector('#form-close');
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.nav-bar');
+let videoBtn = document.querySelectorAll('.vid-btn');
+
+
+window.onsroll = () =>{
+    searchBtn.classList.remove('fa-times');
+    searchBar.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    loginform.classList.remove('active');
+}
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+});
+
+searchBtn.addEventListener('click', () =>{
+    searchBtn.classList.toggle('fa-times');
+    searchBar.classList.toggle('active');
+});
+
+formBtn.addEventListener('click', () =>{
+    loginform.classList.add('active');  
+});
+
+formclose.addEventListener('click', () =>{
+    loginform.classList.remove('active');  
+}); 
+
+videoBtn.forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+        document.querySelector('.controls .active').classList.remove('active');
+        btn.classList.add('active');
+        let src = btn.getAttribute('data-src');
+        document.querySelector('#video-slider').src = src;
+    });
+});
+
+
+$(window).on("hashchange", function () {
+	if (location.hash.slice(1) == "signup") {
+		$(".page").addClass("extend");
+		$("#login").removeClass("active");
+		$("#signup").addClass("active");
+	} else {
+		$(".page").removeClass("extend");
+		$("#login").addClass("active");
+		$("#signup").removeClass("active");
+	}
+});
+
+
+
+
+function validateLoginForm() {
+	var name = document.getElementById("logName").value;
+	var password = document.getElementById("logPassword").value;
+
+	if (name == "" || password == "") {
+		document.getElementById("errorMsg").innerHTML = "Please fill the required fields"
+		return false;
+	}
+
+	else if (password.length < 8) {
+		document.getElementById("errorMsg").innerHTML = "Your password must include atleast 8 characters"
+		return false;
+	}
+	else {
+		alert("Successfully logged in");
+		return true;
+	}
+}
+
+
+function FirstName(fname) {
+    var message = document.getElementsByClassName("error-message");
+    var letters = /^[A-Za-z]+$/;
+    if ( fname =="" || fname.match(letters)) {
+      text="";
+      message[0].innerHTML = text;
+      return true;
+    }
+    else {
+      text="First name should contain only letters";
+      message[0].innerHTML = text;
+      return false;
+    }
+  }
